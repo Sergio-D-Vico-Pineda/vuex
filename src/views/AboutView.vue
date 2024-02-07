4928b35de8906ec575c0f0f3fcb469dd8fd4589e
Sergio David Vico Pineda <servicpin2@alu.edu.gva.es>
2024-02-07 19:50:56 +0100

    about wip

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+   <div>
+      <h1>This is an about page</h1>
+   </div>
 </template>
+
+<style scoped>
+* {
+   color: #f3f4f6;
+}
+</style>
