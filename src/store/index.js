edce2915dd71147798b99b9078494eccf4151c6b
Sergio David Vico Pineda <servicpin2@alu.edu.gva.es>
2024-02-07 19:50:36 +0100

    datos de cards

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,46 @@
-import { createStore } from 'vuex'
+import
+{
+    createStore
+}
+from 'vuex'
 
-export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+export default createStore(
+{
+    state:
+    {
+        cards: [
+        {
+            title: "Vuex",
+            img: "https://picsum.photos/200/200",
+            desc: "Librería de gestión de estado para Vue.js."
+        },
+        {
+            title: "Tailwind",
+            img: "https://picsum.photos/200/200.webp",
+            desc: "Framework de estilos para desarrollo web."
+        },
+        {
+            title: "Vue",
+            img: "https://picsum.photos/200.webp",
+            desc: "Framework progresivo para construir interfaces de usuario."
+        },
+        {
+            title: "Bootstrap",
+            img: "https://picsum.photos/200.jpg",
+            desc: "Framework de front-end para diseño web y móvil."
+        },
+        {
+            title: "Nuxt",
+            img: "https://picsum.photos/200",
+            desc: "Framework de aplicaciones web basado en Vue.js."
+        }]
+    },
+    getters:
+    {},
+    mutations:
+    {},
+    actions:
+    {},
+    modules:
+    {}
 })
