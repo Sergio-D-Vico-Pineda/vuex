73e5aef4ee39b70bed9592ebbb51e016e5e90bd4
Sergio David Vico Pineda <servicpin2@alu.edu.gva.es>
2024-02-07 19:49:24 +0100

    configs format

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+    plugins:
+    {
+        'postcss-import':
+        {},
+        'tailwindcss/nesting':
+        {},
+        tailwindcss:
+        {},
+        autoprefixer:
+        {},
+    },
 }
