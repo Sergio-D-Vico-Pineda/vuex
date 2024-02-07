bfa88fd47bded5bb4b871332512b8dad4309bd12
Sergio David Vico Pineda <servicpin2@alu.edu.gva.es>
2024-02-07 19:49:47 +0100

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,44 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+   <nav class="bg-[#111516] py-4 md:text-xl">
+      <div class=" flex items-center gap-x-14 px-10">
+         <router-link to="/">
+            <img src="./assets/logo.png" width="30" height="30" alt="Inicio">
+         </router-link>
+         <ul class="justify-center items-center flex space-x-6 space-y-0">
+            <li class="text-gray-700 hover:text-gray-900">
+               <router-link to="/">Home</router-link>
+            </li>
+            <li class="text-gray-700 hover:text-gray-900">
+               <router-link to="/about">About</router-link>
+            </li>
+         </ul>
+      </div>
+   </nav>
+   <hr />
+   <router-view />
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+   font-family: Avenir, Helvetica, Arial, sans-serif;
+   -webkit-font-smoothing: antialiased;
+   -moz-osx-font-smoothing: grayscale;
+   text-align: center;
 }
 
-nav {
-  padding: 30px;
+body {
+   background-color: #313638;
 }
 
 nav a {
-  font-weight: bold;
-  color: #2c3e50;
+   color: #98a5b2;
+
+   &:hover {
+      @apply text-blue-300;
+   }
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+   @apply text-blue-500 font-bold;
 }
 </style>
