b0dc62f3dfab892f6f204b27e4adb2727d6a7768
Sergio David Vico Pineda <servicpin2@alu.edu.gva.es>
2024-02-10 14:07:02 +0100

    passing id in home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
    <div class="flex gap-5 flex-wrap justify-center mt-10">
-      <template v-for="card in $store.state.cards">
-         <Card :title="card.title" :desc="card.desc" :img="card.img" />
+      <template v-for="card, index in $store.state.cards">
+         <Card :title="card.title" :desc="card.desc" :img="card.img" :idc="index"/>
       </template>
    </div>
 </template>
